2046124d3a7210fd5973d36fbb7eb21d91ecd08a
XXMD <xxmd3720@gmail.com>
2024-08-26 17:45:10 +0800

    添加xposed模块

---
+++ app/src/main/java/com/example/simple/Tutorial.java
@@ -0,0 +1,11 @@
+package com.example.simple;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class Tutorial implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
